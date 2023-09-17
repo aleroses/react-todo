57816b6fb546a1a59915ea52990c1d4d08bda838
ale vel <ale.vrs@outlook.com>
2023-09-16 20:29:54 -0500

    .

--- src/components/TodoCounter/index.js
+++ src/components/TodoCounter/index.js
@@ -6,10 +6,10 @@ function TodoCounter() {
   const { completedTodos, totalTodos } = React.useContext(TodoContext);
 
   return totalTodos == completedTodos ? (
-    <h1 className="total">Completaste todos los ToDos</h1>
+    <h1 className="total">You completed all the ToDos</h1>
   ) : (
     <h1>
-      Has completado <span className="completed">{completedTodos}</span> de{" "}
+      You have completed <span className="completed">{completedTodos}</span> out of{" "}
       <span className="total">{totalTodos}</span> ToDos
     </h1>
   );
--- src/components/TodoForm/index.js
+++ src/components/TodoForm/index.js
@@ -1,14 +1,28 @@
 import React from "react";
+import { TodoContext } from "../TodoContext";
 import "./TodoForm.css";
 
 function TodoForm() {
+  const { addTodo, setOpenModal } = React.useContext(TodoContext);
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    addTodo(newTodoValue);
+    setOpenModal(false);
+  };
+
+  const [newTodoValue, setNewTodoValue] = React.useState("");
+
+  const onCancel = () => {
+    setOpenModal(false);
+  };
+
+  const onChange = (event) => {
+    setNewTodoValue(event.target.value);
+  };
+
   return (
-    <form
-      onSubmit={(event) => {
-        event.preventDefault();
-      }}
-      action=""
-    >
+    <form onSubmit={onSubmit} action="">
       <label htmlFor="">Write a new ToDo</label>
       <textarea
         name=""
@@ -16,12 +30,14 @@ function TodoForm() {
         cols="30"
         rows="10"
         placeholder="Write something..."
+        value={newTodoValue}
+        onChange={onChange}
       />
       <div className="buttons">
-        <button type="" className="TodoForm cancel">
+        <button type="button" className="TodoForm cancel" onClick={onCancel}>
           Cancel
         </button>
-        <button type="" className="TodoForm save">
+        <button type="submit" className="TodoForm save">
           Save
         </button>
       </div>
